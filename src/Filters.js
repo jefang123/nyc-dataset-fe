1d1c8f37f356955c68d4da2725aca4008502a1e3
Jeff <jeffreyfang123@gmail.com>
2020-05-11 11:11:09 +0900

    Update Filters

--- src/Filters.js
+++ src/Filters.js
@@ -1,23 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import { Info } from './Info.js';
 
+const defaultState = {}
 
 class Dataset extends Component {
-  state = {};
-  // onSubmit = () => 
-  //   fetch(`/api/${this.props.match.params.dataset}`)
-  //   .then(res => res.json())
-  //   .then(data => {
-  //     let { dataset_name, total, columns } = data
-  //     this.setState({dataset_name, count:total, columns})
-  //   })
+  state = defaultState;
+
+  onChange = (key, value) => 
+    this.setState({key:value})
+
+  onSubmit = () => 
+    fetch(`/api/${this.props.match.params.dataset}`, json=this.state)
+    .then(res => res.json())
+    .then(data => {
+      let { dataset_name, total, columns } = data
+      this.setState({dataset_name, count:total, columns})
+    })
 
   render() {
     const { dataset_name, count, columns } = this.state;
-    return (
+    const Map = Object.keys(defaultState).map(key) =>
       <>
+        <Input></Input>
+        <Button></Button>
       </>
+    return (
+      {Map}
     )
   }
 }
