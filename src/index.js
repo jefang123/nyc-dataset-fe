477166241ba7238ac7cc9c92db02c846f55cc03b
Jeff <jeffreyfang123@gmail.com>
2020-05-15 11:15:45 +0900

    Update store in index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 import { App } from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <App store={configureStore()}/>,
   document.getElementById('root')
 );
