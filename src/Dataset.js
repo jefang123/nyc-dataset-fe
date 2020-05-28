8463e9d02c8b1ba54f54525e410f350750ac7866
Jeff <jeffreyfang123@gmail.com>
2020-05-28 09:49:25 +0900

    Update dataset

--- src/Dataset.js
+++ src/Dataset.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import { Info } from './Info.js';
 
 class Dataset extends Component {
   state = {dataset_name:"test_dataset", count: 555};
   componentDidMount = () => {
     this.props.fetchInfo(this.props.match.params.dataset)
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.match.params.dataset !== prevProps.match.params.dataset) {
+      let { dataset_name, total, columns } = this.props
+      this.setState({
+        dataset_name,
+        total,
+        columns
+      })
+    }
   }
   // onSubmit = () => 
   //   fetch(`/api/${this.props.match.params.dataset}`)
